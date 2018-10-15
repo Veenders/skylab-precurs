075feb121b8fb108537247cd492a9170ce1403ee
LAPTOP-1PMLHPQD\Carles Vila <carles@grupovica.com>
2018-10-15 16:58:58 +0200

    migdia 15-10

--- tema4/tema4.js
+++ tema4/tema4.js
@@ -5,6 +5,7 @@ function showNums() {
         console.log(i + 1 + "ª pareja " + nums[i] * 2 + " - " + nums[i + 1] * 2);
     }
 }
+console.log("Ejercicio A");
 showNums();
 
 //a1) La funcion debería aceptar la array a tratar como argumento
@@ -14,34 +15,241 @@ function showNumsArg() {
         console.log(i + 1 + "ª pareja " + nums[i] * 2 + " - " + nums[i + 1] * 2);
     }
 }
+console.log("Ejercicio A1")
 showNumsArg(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 //a2) Pasa también el numero a multiplicar a la función como argumento
+function showNumsMult() {
+    var nums = arguments;
+    for (var i = 0; i < nums.length - 2; i++) {
+        console.log(i + 1 + "ª pareja " + nums[i] * nums[nums.length - 1] + " - " + nums[i + 1] * nums[nums.length - 1]);
+    }
+}
+console.log("Ejercicio A2")
+showNumsMult(1, 2, 3, 4, 5, 6, 7, 8, 9, 5);
 
 //a3) La función debería ser capaz de recibir el numero de parejas que queremos devolver del total.
+function showNumsLimit() {
+    var nums = arguments;
+    for (var i = 0; i < nums.length - 3 && i < nums[nums.length-2]; i++) {
+        console.log(i + 1 + "ª pareja " + nums[i] * nums[nums.length - 1] + " - " + nums[i + 1] * nums[nums.length - 1]);
+    }
+}
+console.log("Ejercicio A3")
+showNumsLimit(1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 5);
 
 //b) Volvemos a la numeros... Podrias hacer una funcion que mostrara por pantalla la serie Fibonacci?
+function fibonacciNums(){
+    var fibonaccinum=[];
+    for(var i =0; i<30;i++){
+        fibonaccinum.push(i>1?fibonaccinum[i-2]+fibonaccinum[i-1]:i);
+    }
+    return fibonaccinum.join(" ");
+}
+console.log("Ejercicio B");
+console.log(fibonacciNums());
 
 //b2) Puedes añadir además, la posición de cada resultado?
+function fibonacciNumsPosition(){
+    var fibonaccinum=[];
+    var message="";
+    for(var i =0; i<30;i++){
+        fibonaccinum.push(i>1?fibonaccinum[i-2]+fibonaccinum[i-1]:i);
+        message += i+1+"º pos - "+fibonaccinum[i]+", ";
+    }
+    return message;
+}
+console.log("Ejercicio B2");
+console.log(fibonacciNumsPosition());
 
 //b3) Ahora, inserta los resultados en una array y muestralos todos juntos de una manera amigable.
+function fibonacciArray(){
+    var fibonaccinum=[];
+    for(var i =0; i<30;i++){
+        fibonaccinum.push(i>1?fibonaccinum[i-2]+fibonaccinum[i-1]:i);
+    }
+    return fibonaccinum;
+}
+console.log("Ejercicio B3");
+console.table(fibonacciArray());
 
 //b4) Ahora, el usuario debería ser capaz de especificar la posición de la serie hasta donde queremos llegar.
+function fibonacciNumsPosition(pos){
+    var fibonaccinum=[];
+    var message="";
+    for(var i =0; i<pos;i++){
+        fibonaccinum.push(i>1?fibonaccinum[i-2]+fibonaccinum[i-1]:i);
+        message += i+1+"º pos - "+fibonaccinum[i]+", ";
+    }
+    return message;
+}
+console.log("Ejercicio B4");
+console.log(fibonacciNumsPosition(10));
+console.log(fibonacciNumsPosition(50));
+console.log(fibonacciNumsPosition(100));
 
 //b5) Ahora, muestra los resultados en forma piramidal:
+function fibonacciArray(pos){
+    var fibonaccinum=[];
+    for(var i =0; i<pos;i++){
+        fibonaccinum.push(i>1?fibonaccinum[i-2]+fibonaccinum[i-1]:i);
+        console.log(fibonaccinum.join(" "));
+    }
+    for(var i=pos; i>0;i--){
+        fibonaccinum.pop(i);
+        console.log(fibonaccinum.join(" "));
+    }
+}
+console.log("Ejercicio B5");
+fibonacciArray(10);
 
 //c) Simple Scripting program. Crea un programa que transforme un número de 4 dígitos en otro diferente con las posiciones de los dígitos cambiadas, creandio un nuevo código
-
+var code = 3712;
+function codeScript(code){
+    var coded = [];
+    var codeo = code.toString().split("");
+    for(var i=0;i<codeo.length;i++){
+        if (i==codeo.length-1){
+            coded[0]=codeo[i];
+         }else{
+             coded[i+1]=codeo[i];
+         }
+    }
+    return parseInt(coded.join(""));
+}
+console.log("Ejercicio C");
+console.log(codeScript(code));
 //c2) Ahora, el usuario debería poder introducir como parámetro dos códigos a la vez y devolver los dos códigos encriptados (Los dos códigos se deberían encriptar en la misma función)
-
+var code = 3712;
+function codeScript2(code1,code2){
+    var coded1 = codeScrpit(code1);
+    var coded2 = codeScrpit(code2);
+    return [coded1,coded2];
+}
+console.log("Ejercicio C2");
+console.log(codeScript(code));
 //c3) Ahora, vamos a añadir un nivel más de seguridad. Despues de cambiar la posición de los dígitos, multiplicaremos a cada miembro por un numero cuya multiplicación no sea superior a 10. (Si es superior a 10, conseguiremos una multplicación de dos digitos y el código ya no sería de 4 valores)
-
+function Encrypt(code){
+    var coded = [];
+    var codeo = code.toString().split("");
+    for(var i=0;i<codeo.length;i++){
+        if (i==codeo.length-1){
+            coded[0]=10-parseInt(codeo[i]);
+         }else{
+             coded[i+1]=10-parseInt(codeo[i]);
+         }
+    }
+    return parseInt(coded.join(""));
+}
+var key=[3,2,4,5];
+function EncryptSubs(code,key){
+    var coded = code+key;
+    if(coded>10){
+        coded = coded-10;
+    }
+    return coded;
+}
+function EncryptVigenere(code){
+    var coded = [];
+    var codeo = code.toString().split("");
+    var j = 0;
+    for(var i=0;i<codeo.length;i++){
+        if (i==codeo.length-1){
+            coded[0]=EncryptSubs(parseInt(codeo[i]),key[j]);
+         }else{
+             coded[i+1]=EncryptSubs(parseInt(codeo[i]),key[j]);
+         }
+         j==key.length-1?j=0:j++;
+    }
+    return parseInt(coded.join(""));
+}
+console.log("Ejercicio C3");
+var coded = Encrypt(code)
+console.log("Coded to 10: "+coded);
+var codedV = EncryptVigenere(code);
+console.log("Coded by Vigenere: "+codedV);
 //c4) Ahora, implementa en otra funcion aparte el decrypter(), que recibirá como argumento un código encriptado (y correspondientemente multiplicado en el apartado c3) y nos devuelva el código desencriptado.
-
+function Decrypt(code){
+    var coded = [];
+    var codeo = code.toString().split("");
+    for(var i=0;i<codeo.length;i++){
+        if (i==codeo.length-1){
+            coded[i]=10-parseInt(codeo[0]);
+         }else{
+             coded[i]=10-parseInt(codeo[i+1]);
+         }
+    }
+    return parseInt(coded.join(""));
+}
+function DecryptSubs(code,key){
+    var coded = code-key;
+    if(coded<0){
+        coded = coded+10;
+    }
+    return coded;
+}
+function DecryptVigenere(code){
+    var coded = [];
+    var codeo = code.toString().split("");
+    var j = 0;
+    for(var i=0;i<codeo.length;i++){
+        if (i==codeo.length-1){
+            coded[i]=DecryptSubs(parseInt(codeo[0]),key[j]);
+         }else{
+             coded[i]=DecryptSubs(parseInt(codeo[i+1]),key[j]);
+         }
+         j==key.length-1?j=0:j++;
+    }
+    return parseInt(coded.join(""));
+}
+console.log("Ejercicio C4");
+console.log("Decoded to 10: "+Decrypt(coded));
+console.log("Decoded by Vigenere: "+DecryptVigenere(codedV));
 //c5) Añade las dos funciones a la misma función padre, de forma que encripte y desencripte a la vez cuando termine de ejecutarse.
 
+function Crypt(code){
+    var coded = Encrypt(code);
+    console.log("El código encriptado es: "+coded);
+    var decoded = Decrypt(coded);
+    console.log("El código desencriptado es: "+decoded);
+    return decoded;
+}
+function CryptVigenere(code){
+    var coded = EncryptVigenere(code);
+    console.log("El código encriptado por Vigenere es: "+coded);
+    var decoded = DecryptVigenere(coded);
+    console.log("El código desencriptado Vigenere es: "+decoded);
+    return decoded;
+}
+console.log("Ejercicio C5");
+console.log(Crypt(code));
+console.log(CryptVigenere(code));
 //c6) El usuario podrá solo introducir letras, cada número del 0 al 9 corresponderá a varias letras. Podéis seguir este esquema.
+var dictionary = {
+    0: ['A', 'K', 'T', 'F', 'O', 'Y'],
+    1: ['B', 'L', 'U', 'G', 'P', 'Z'],
+    2: ['C', 'M', 'V', 'H', 'Q', '.'],
+    3: ['D', 'N', 'W', 'I', 'R', ','],
+    4: ['E', 'Ñ', 'X', 'J', 'S', ' ']
+}
+function codeLetter(code){
+    for(var i=0;i<5;i++){
+        var exists=dictionary[i].indexOf(code);
+        if(exists!=-1){
+            return [i,exists];
+        }
+    }    
+}
+function letterCoded(pos){
+    this.line=pos[0];
+    this.col=pos[1];
+    this.ReturnLetter = function (){
+        return dictionary[this.line][this.col];
+    }
+}
 
+console.log("Ejercicio C6")
+console.log(codeWords("H"));
 //d) Crea un programa que use la encriptacion Romana, como es? Si tenemos la palabra SKYLAB, la palabra encriptada será SLKAYB.
 
 //d2) Programa el desencriptador, pasa como parámetro SLKAYB y que devuelva SKYLAB.
